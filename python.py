42594465fc22e2834e73ff2101ca5dd37d09ecaf
Sedatoztep <65872570+Sedatoztep@users.noreply.github.com>
2020-08-11 01:28:29 +0300

    pythonda string formatlama

---
+++ python.py
@@ -0,0 +1,7 @@
+name='Sedat'
+surname='Öztep'
+
+print('My name is {} {}'.format(name,surname))
+print('My name is {0} {1}'.format(name,surname))
+print('My name is {s} {n}'.format(n=name,s=surname))
+print("My name is {} {} and I'm {} years old".format(name,surname,'23'))
